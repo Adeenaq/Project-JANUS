a463f092fb2abe6427c16c1b32e6295bad9ca009
Danish Humair <me@danishhumair.com>
2024-07-01 23:16:28 +0500

    fixed issue with background parallax script

--- Assets/Scripts/BackgroundParallaxEffect.cs
+++ Assets/Scripts/BackgroundParallaxEffect.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEditor.Experimental.AssetImporters;
 
 public class BackgroundsParallaxEffect : MonoBehaviour
 {
